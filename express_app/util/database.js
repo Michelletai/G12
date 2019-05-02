33a9044d2d962c2130925cc5352f41793588ce0e
tommy <cy94295@gmail.com>
2019-05-02 17:22:28 +0800

    修改 設定

--- express_app/util/database.js
+++ express_app/util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'momovnc.sytes.net',
-    user: 'Harry',
-    database: 'db_404411687',
-    password: 'sqlclass2018'
+    host: 'localhost',
+    user: 'root',
+    database: 'ikea',
+    password: 'k3241234'
 });
 
 module.exports = pool.promise();
