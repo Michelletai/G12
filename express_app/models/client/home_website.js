3656e600c414557412187faf3c1cf6339452dad3
huangge0513 <cy94295@gmail.com>
2019-05-20 03:24:38 +0800

    新增 前端畫面 初稿

--- express_app/models/client/home_website.js
+++ express_app/models/client/home_website.js
@@ -3,6 +3,6 @@ const db = require('../../util/client_home');
 module.exports = class website {
   // READ
   static fetchAll() {
-    return db.execute('SELECT url.id,url.name,url.url,`desc`.desc FROM home.websiteurl url left join home.websitedesc `desc` on url.name = `desc`.name ');
+    return db.execute('SELECT url.id,url.realname,url.name,url.url,`desc`.desc FROM home.websiteurl url left join home.websitedesc `desc` on url.realname = `desc`.realname ');
   }
 };
