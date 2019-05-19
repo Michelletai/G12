7555a696a907e5275807013ad66c661408d9ee34
huangge0513 <cy94295@gmail.com>
2019-05-19 23:48:41 +0800

    前端 編輯中

--- express_app/models/client/home_websiteurl.js
+++ express_app/models/client/home_websiteurl.js
@@ -2,7 +2,7 @@ const db = require('../../util/client_home');
 
 module.exports = class website {
     // READ
-  static fetchAll() {
+    static fetchAll() {
     return db.execute('SELECT * FROM home.websiteurl;');
   }
 
